e55bd562ede5e489d384da19c7fcb1e9687267ee
Nick Anderson <nanderson@reliaslearning.com>
2017-12-01 08:01:17 -0500

    Initial commit

---
+++ AdventOfCode2017/Day1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2017
+{
+    public class Day1
+    {
+        public void Run()
+        {
+            
+        }
+    }
+}
---
+++ AdventOfCode2017/Program.cs
@@ -0,0 +1,12 @@
+﻿
+namespace AdventOfCode2017
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var day = new Day1();
+            day.Run();
+        }
+    }
+}
