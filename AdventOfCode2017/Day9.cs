bb0c5094833802a778b1939f0826ab78537c224c
Nick Anderson <njcanderson@gmail.com>
2017-12-09 19:08:49 -0500

    Day 9 solution

---
+++ AdventOfCode2017/Day9.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+
+namespace AdventOfCode2017
+{
+    public class Day9
+    {
+        private readonly string _input = File.ReadAllText(@"Input/Day9.txt");
+        public void Run()
+        {
+            PartOneAndTwo();
+        }
+
+        private void PartOneAndTwo()
+        {
+            var score = 0;
+            var depth = 0;
+            var garbageCount = 0;
+            var garbage = false;
+            char letter;
+
+            for(var i = 0; i < _input.Length; i++)
+            {
+                letter = _input[i];
+                if(letter == '!')
+                {
+                    i++;
+                }
+                else
+                {
+                    if(letter == '{' && !garbage)
+                    {
+                        depth++;
+                        score += depth;
+                    }
+                    if(letter == '}' && !garbage)
+                    {
+                        depth--;
+                    }
+                    if (letter == '>')
+                    {
+                        garbage = false;
+                    }
+                    if (garbage)
+                    {
+                        garbageCount++;
+                    }
+                    if (letter == '<')
+                    {
+                        garbage = true;
+                    }
+                }
+            }
+            Console.WriteLine(score);
+            Console.WriteLine(garbageCount);
+        }
+    }
+}
--- AdventOfCode2017/Program.cs
+++ AdventOfCode2017/Program.cs
@@ -5,7 +5,7 @@ namespace AdventOfCode2017
     {
         public static void Main(string[] args)
         {
-            var day = new Day8();
+            var day = new Day9();
             day.Run();
         }
     }
