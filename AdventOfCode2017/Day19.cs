8a78350d517067702d2565397134ab71943f9ca6
Nick Anderson <nanderson@reliaslearning.com>
2017-12-19 08:29:14 -0500

    Day 19 start

---
+++ AdventOfCode2017/Day19.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2017
+{
+    public class Day19
+    {
+        private readonly string[][] _path = File.ReadAllLines(@"Input/Day19.txt")
+            .Select(x => x.ToCharArray().Select(y => y.ToString()).ToArray()).ToArray();
+        public void Run()
+        {
+            PartOne();
+            PartTwo();
+        }
+
+        private void PartOne()
+        {
+            var direction = "down";
+            var letters = "";
+            var startingSpace = Array.IndexOf(_path[0], "|");
+            var i = 0;
+            var j = startingSpace;
+            var finished = false;
+            while (!finished)
+            {
+                switch (direction)
+                {
+                    case "down":
+                        i++;
+                        if (_path[i][j] != "|")
+                        {
+                            
+                        }
+                        break;
+                }
+            }
+        }
+
+        private void PartTwo()
+        {
+            
+        }
+    }
+}
--- AdventOfCode2017/Program.cs
+++ AdventOfCode2017/Program.cs
@@ -5,7 +5,7 @@ namespace AdventOfCode2017
     {
         public static void Main(string[] args)
         {
-            var day = new Day18();
+            var day = new Day19();
             day.Run();
         }
     }
