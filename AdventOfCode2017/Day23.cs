292d22c0ada02049b244c3ad0492129445c53d61
Nick Anderson <njcanderson@gmail.com>
2017-12-23 10:11:54 -0500

    Day 23 solution

--- AdventOfCode2017/Day23.cs
+++ AdventOfCode2017/Day23.cs
@@ -4,9 +4,9 @@ using System.IO;
 
 namespace AdventOfCode2017
 {
-    public class Day18
+    public class Day23
     {
-        private readonly string[] _instructions = File.ReadAllLines(@"Input/Day18.txt");
+        private readonly string[] _instructions = File.ReadAllLines(@"Input/Day23.txt");
         public void Run()
         {
             PartOne();
@@ -16,7 +16,7 @@ namespace AdventOfCode2017
         private void PartOne()
         {
             var registers = new Dictionary<string, long>();
-            long lastSoundPlayed = 0;
+            long mulCount = 0;
             for (long i = 0; i < _instructions.Length; i++)
             {
                 var split = _instructions[i].Split(' ');
@@ -29,9 +29,6 @@ namespace AdventOfCode2017
                 }
                 switch (split[0])
                 {
-                    case "snd":
-                        lastSoundPlayed = registers[split[1]];
-                        break;
                     case "set":
                         if (long.TryParse(split[2], out var setValue))
                         {
@@ -42,14 +39,14 @@ namespace AdventOfCode2017
                             registers[split[1]] = registers[split[2]];
                         }
                         break;
-                    case "add":
+                    case "sub":
                         if (long.TryParse(split[2], out var addValue))
                         {
-                            registers[split[1]] += addValue;
+                            registers[split[1]] -= addValue;
                         }
                         else
                         {
-                            registers[split[1]] += registers[split[2]];
+                            registers[split[1]] -= registers[split[2]];
                         }
                         break;
                     case "mul":
@@ -61,28 +58,12 @@ namespace AdventOfCode2017
                         {
                             registers[split[1]] *= registers[split[2]];
                         }
+                        mulCount++;
                         break;
-                    case "mod":
-                        if (long.TryParse(split[2], out var modValue))
-                        {
-                            registers[split[1]] %= modValue;
-                        }
-                        else
-                        {
-                            registers[split[1]] %= registers[split[2]];
-                        }
-                        break;
-                    case "rcv":
-                        if (registers[split[1]] > 0)
-                        {
-                            Console.WriteLine(lastSoundPlayed);
-                            return;
-                        }
-                        break;
-                    case "jgz":
+                    case "jnz":
                         if (long.TryParse(split[1], out var regValue))
                         {
-                            if (regValue > 0)
+                            if (regValue != 0)
                             {
                                 if (long.TryParse(split[2], out var jumpValue))
                                 {
@@ -96,7 +77,7 @@ namespace AdventOfCode2017
                         }
                         else
                         {
-                            if (registers[split[1]] > 0)
+                            if (registers[split[1]] != 0)
                             {
                                 if (long.TryParse(split[2], out var jumpValue))
                                 {
@@ -111,11 +92,51 @@ namespace AdventOfCode2017
                         break;
                 }
             }
+            Console.WriteLine(mulCount);
         }
 
         private void PartTwo()
         {
-            
+            int a = 1;
+            int b = 0;
+            int c = 0;
+            int d = 0;
+            int e = 0;
+            int f = 0;
+            int g = 0;
+            int h = 0;
+            b = 57;
+            c = b;
+
+            if (a != 0)
+            {
+                b *= 100;
+                b += 100000;
+                c = b;
+                c += 17000;
+            }
+            do
+            {
+                f = 1;
+                d = 2;
+                e = 2;
+
+                for (d = 2; d < b; d++)
+                {
+                    if (b % d == 0)
+                    {
+                        f = 0;
+                        break;
+                    }
+                }
+                if (f == 0)
+                {
+                    h++;
+                }
+                g = b - c;
+                b += 17;
+            } while (g != 0);
+            Console.WriteLine(h);
         }
     }
 }
